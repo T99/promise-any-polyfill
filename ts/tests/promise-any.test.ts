12538eba89fe8234db6eaf575a73df1f9bd7b56e
Trevor Sears <trevors@knorrmarketing.com>
2021-09-10 13:09:00 -0400

    Added copyright headers.

--- ts/tests/promise-any.test.ts
+++ ts/tests/promise-any.test.ts
@@ -2,6 +2,22 @@
  * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
  * 11:46 PM -- October 28th, 2019.
  * Project: promise-any-polyfill
+ * 
+ * promise-any-polyfill - A polyfill for the proposed/upcoming `Promise.any` functionality.
+ * Copyright (C) 2021 Trevor Sears
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import "../implementation";
@@ -64,8 +80,6 @@ test("Rejecting Promises should not effect the acted-upon value so long as some 
 test("If all Promises reject, Promise.any should reject.", 
 	async (): Promise<void> => {
 	
-	console.log(`hello world`);
-		
 	let promises: Array<Promise<string>> = [
 		getTimedRejectingPromise(25, "ignore me"),
 		getTimedRejectingPromise(50, "yes"),
