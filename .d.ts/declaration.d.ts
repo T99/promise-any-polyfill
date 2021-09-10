aa60c9a307aefb1ebd022d60f11404fb4c5221ed
Trevor Sears <trevors@knorrmarketing.com>
2021-09-10 10:52:29 -0400

    Removed `.d.ts/`, `js/`, and `ts/tests/coverage/` directories.

--- .d.ts/declaration.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type TypeOrPromiseLike<T> = T | PromiseLike<T>;
-export declare type ReturnResultOrPromiseLike<T> = T extends PromiseLike<infer U> ? U : T;
-declare global {
-    interface PromiseConstructor {
-        any<T = any, R extends ReturnResultOrPromiseLike<T> = ReturnResultOrPromiseLike<T>>(values: Iterable<TypeOrPromiseLike<T>>): Promise<R>;
-    }
-}
